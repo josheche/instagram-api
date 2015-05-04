5c7d113cc9451d0af233d7aceb5429c9d5c44dfc
Joshua Echeverri <josheche@Gmail.com>
2015-05-04 16:55:29 -0400

    added instagram api

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,9 @@
+require “instagram”
+
+Instagram.configure do |config|
+
+  config.client_id = "2efdf3b1eb284e4588d2ce7fad2c0f0c"
+  config.access_token = "f955cb4445c8439796f6dbce16601bf7"
+
+end
+
