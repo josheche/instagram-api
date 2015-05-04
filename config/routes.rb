46085c08a8f3f017c8fbf79298ca27475ebbacfd
Joshua Echeverri <josheche@Gmail.com>
2015-05-04 19:53:49 -0400

    added omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  root 'instagram#index'
+  root 'home#index'
+
+  get '/auth/:provider/callback', to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
