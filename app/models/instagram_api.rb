00d82c2cae4f4ed58d9eec42b8e6cac17deeeb56
Rachel Metheny <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-05-07 17:13:21 -0400

    data mined 1stimage, made videos work, MAJOR REFACTORING

--- app/models/instagram_api.rb
+++ app/models/instagram_api.rb
@@ -1,3 +1,14 @@
 class InstagramAPI
+	def initialize(tag)
+		response = RestClient.get "https://api.instagram.com/v1/tags/#{tag}/media/recent?client_id=2efdf3b1eb284e4588d2ce7fad2c0f0c"
+		@data = JSON.parse(response)
+	end
 
+	def first_image_url
+		get_media.first["images"]["standard_resolution"]["url"]
+	end
+
+	def get_media
+		@data['data']
+	end
 end
